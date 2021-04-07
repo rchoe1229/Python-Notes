a8f882339c09357745ab5d852fb9adab1ebd0cd1
rchoe1229 <ryan.choe@outlook.com>
2021-04-07 17:02:30 -0600

    Initial Commit

---
+++ practice.py
@@ -0,0 +1,21 @@
+print("Hello World")
+# output => Hello World
+
+d = {"k1": {"innerkey": [1, 2, 3]}}
+print(d["k1"]["innerkey"])
+# output => [1, 2, 3]
+
+# Boolean
+True or False
+
+# Array
+my_list = [1, 2, 3]
+
+# Tuples -  Similar to array, but uses parenthesis and is IMMUTABLE
+my_list = (1, 2, 3)
+
+# Set - Similar to dictionary, but only contains unique elements
+print({1,1,1,2,2,3,4,5,6})
+# output => {1,2,3,4,5,6}
+
+# Comparison Operators 
