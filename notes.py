8ddcc970b993993f7006ff39c50f1669ce294dd7
rchoe1229 <ryan.choe@outlook.com>
2021-04-08 12:02:28 -0600

    Added Other Calls

--- notes.py
+++ notes.py
@@ -1,26 +1,27 @@
-# Displaying information w/ print
+
+#! Displaying information w/ print
 print("Hello World")
 # output => Hello World
 
-# Dictionary and indexing
+#! Dictionary and indexing
 d = {"k1": {"innerkey": [1, 2, 3]}}
 print(d["k1"]["innerkey"])
 # output => [1, 2, 3]
 
-# Boolean
+#! Boolean
 True or False
 
-# Array
+#! Array
 my_list = [1, 2, 3]
 
-# Tuples -  Similar to array, but uses parenthesis and is IMMUTABLE
+#! Tuples -  Similar to array, but uses parenthesis and is IMMUTABLE
 my_list = (1, 2, 3)
 
-# Set - Similar to dictionary, but only contains unique elements
+#! Set - Similar to dictionary, but only contains unique elements
 print({1, 1, 1, 2, 2, 3, 4, 5, 6})
 # output => {1,2,3,4,5,6}
 
-# Comparison Operators
+#! Comparison Operators
 print(1 >= 2)
 print(1 <= 2)
 print(1 == 2)
@@ -28,7 +29,7 @@ print(1 == 1)
 print(1 == 1 and 2 < 3)
 print(1 == 1 or 2 > 3)
 
-# If Elif Else
+#! If Elif Else
 if 1 < 2:
   print('Hell Yeah!')
 
@@ -42,10 +43,8 @@ else:
 # Hell Yeah! is true because 1 is less than 2, so would print the string under. 
 # Threes! is true because 1 != 2, so the next condition is if 3 == 3, which is true. This prints the string underneath
 
-# For Loops
-
+#! For Loops
 sequence = [1,2,3,4,5]
-
 for num in sequence:
   print(num)
 # output => 1
@@ -54,8 +53,7 @@ for num in sequence:
 # output => 4
 # output => 5
 
-# While Loops
-
+#! While Loops
 i = 1
 while i < 5:
   print('i is:{}'.format(i))
@@ -65,7 +63,7 @@ while i < 5:
 # output => i is: 3
 # output => i is: 4
 
-#  Range
+#! Range
 for num in range(0,5):
   print(num)
 # output => 0
@@ -76,7 +74,7 @@ for num in range(0,5):
 print(list(range(10)))
 # output => [0,1,2,3,4,5,6,7,8,9]
 
-# List Comprehension (First example is with a for loop and second is list comprehension, both do the same)
+#! List Comprehension (First example is with a for loop and second is list comprehension, both do the same)
 x = [1,2,3,4]
 out = []
 for num in x:
@@ -90,7 +88,7 @@ List comprehension basically took the for loop and logically did it backwards
 Every number in our x array will be squared is how line 84 is read
 """
 
-# Functions (names should be lower case, functions also take in parameter/s)
+#! Functions (names should be lower case, functions also take in parameter/s)
 def my_func(name):
   print('Hello ' + name)
 
@@ -121,7 +119,7 @@ for num in score:
 # output => 4
 # output => 5
 
-# Map/Filter and Lamda Expressions
+#! Map/Filter and Lamda Expressions
 def times2(var):
   return var*2
 """ 
@@ -150,4 +148,27 @@ print(
 print(
   list(filter(lambda num: num%2 == 0, my_list))
 )
+# output => [2, 4] this filter showed only values that were truthful and returned only even values
 
+#! Other calls
+s = 'hello my name is Ryan'
+print(s.lower())
+# output => hello my name is ryan
+print(s.upper())
+# output => HELLO MY NAME IS RYAN
+print(s.split())
+# output => ['hello', 'my', 'name', 'is', 'Ryan']
+print(my_list.pop())
+# output => 5
+print(my_list)
+# output => [1, 2, 3, 4]
+my_list.append(5)
+print(my_list)
+# output => [1, 2, 3, 4, 5]
+
+#! Dictionary Calls
+d = {'k1':1,'k2':2 }
+print(d.items())
+# output => dict_items([('k1', 1), ('k2', 2)])
+print(d.values())
+# output => dict_values([1, 2])
